0c690cd145e5746730af66701523755bfbfb4b64
Justin Merz <jrmerz@gmail.com>
2017-07-26 11:40:29 -0700

    quick fix

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 module.exports = {
   BaseModel : require('./lib/BaseModel'),
   EventBus : require('./lib/EventBus'),
-  ReduxStore : require('./lib/redux/store'),
-  ReduxCombineReducers : require('redux').combineReducers,
-  ReduxObserver : require('redux-observers').observer,
-  ReduxAPIMiddleware : require('./lib/redux/middleware/api')
+  BaseStore : require('./lib/BaseStore')
 }
