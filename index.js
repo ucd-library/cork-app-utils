fa1a8bc388a784632f3fcddd8df6efaf97e04f63
Justin Merz <jrmerz@gmail.com>
2017-07-27 11:35:15 -0700

    adding StoreServiceWrapper

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   BaseModel : require('./lib/BaseModel'),
   EventBus : require('./lib/EventBus'),
-  BaseStore : require('./lib/BaseStore')
+  BaseStore : require('./lib/BaseStore'),
+  StoreServiceWrapper : require('./lib/StoreServiceWrapper')
 }
