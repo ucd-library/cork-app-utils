84a39324d18277146bba330fff8bb3fec8cf0f82
Justin Merz <jrmerz@gmail.com>
2017-07-27 12:11:47 -0700

    adding BaseService class

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
-  BaseModel : require('./lib/BaseModel'),
   EventBus : require('./lib/EventBus'),
+  BaseModel : require('./lib/BaseModel'),
   BaseStore : require('./lib/BaseStore'),
+  BaseService : require('./lib/BaseService'),
   StoreServiceWrapper : require('./lib/StoreServiceWrapper')
 }
