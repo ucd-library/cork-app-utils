1b00a628bde8803e49079264b6e133c97b12d3b1
Justin Merz <jrmerz@gmail.com>
2024-09-18 14:37:10 -0700

    adding global storage and usage of Registry and EventBus, new helpers for stores

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ import LitCorkUtils from './lib/LitCorkUtils.js';
 import LruStore from './lib/lru-store.js';
 import PayloadUtils from './lib/PayloadUtils.js';
 import {getLogger, setLoggerConfig} from './lib/logger.js';
+import {setGlobal} from './lib/globals.js';
+
+setGlobal('lib', {
+  BaseModel, BaseStore, BaseService,
+  BaseMixin, Mixin, EventBus, LitCorkUtils,
+  Registry, LruStore, getLogger, setLoggerConfig,
+  PayloadUtils
+})
 
 export {
   BaseModel, BaseStore, BaseService,
